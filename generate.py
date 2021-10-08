fe4282d131d0009b808daeff8bb986601432cc98
ZoukiDev <zouki.dev@gmail.com>
2021-10-08 07:50:37 +0200

    Update

---
+++ generate.py
@@ -0,0 +1,27 @@
+import random
+import PIL
+from PIL import Image, ImageColor
+
+bgColors = [
+    ImageColor.getcolor('#3498db', 'RGB'),
+    ImageColor.getcolor('#f1c40f', 'RGB'),
+    ImageColor.getcolor('#e74c3c', 'RGB'),
+    ImageColor.getcolor('#2ecc71', 'RGB'),
+]
+
+faces = [
+    'assets/faces/001.png'
+]
+
+eyes = [
+    'assets/eyes/001.png'
+]
+
+new_image = Image.new('RGBA', (32, 32), random.choice(bgColors))
+
+random_face = Image.open(random.choice(faces))
+# random_eye = Image.open(random.choice(eyes))
+new_image.paste(random_face)
+# new_image.paste(random_eye)
+
+new_image.save('new_image.png')
