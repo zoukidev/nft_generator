64a5d20073bb892caa8f2771f4aabe8733915144
Melvin C <zouki.dev@gmail.com>
2021-10-20 15:05:24 +0200

    Update

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ from src.profil import Profile
 from src.loader import load_assets
 
 parser = argparse.ArgumentParser(description='Generate random NFT.')
-parser.add_argument('-n','--number', help='The number of NFTs you wish to generate', default=1, required=False)
+parser.add_argument('-n','--number', help='The number of NFTs you wish to generate', default=1, required=True)
 args = parser.parse_args()
 
 load_assets([
@@ -27,4 +27,6 @@ if __name__ == '__main__':
         yymmss = datetime.datetime.now().strftime('%y-%m-%s')
         datehash = hashlib.md5('{}-{}'.format(random.randrange(0, 9999), yymmss).encode('utf-8')).hexdigest()
         new_profile = Profile()
-        new_profile.saveImage('nft/{}.png'.format(datehash))
+        filename = 'nft/{}.png'.format(datehash)
+        print(filename)
+        new_profile.saveImage(filename)
